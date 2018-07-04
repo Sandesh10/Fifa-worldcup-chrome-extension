39ae03c0bcd6dccaf3db6353a091123bb51a612e
Sandesh <timilsina.sandesh@gmail.com>
2018-07-04 13:29:13 +0545

    Readme file updated

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# World-cup-chrome-extension
+# Scoreboard
+
+Chrome extension to get world cup score updates in your browser
+
+	![Working Demo](https://github.com/Sandesh10/Scoreboard/blob/master/demo.gif)
+
+
+## Getting Started
+
+### Installing
+- Clone the repo
+       
+    ```
+    git clone https://github.com/Sandesh10/Scoreboard.git
+    ```
+    ### Installing on Chrome 
+    * Load the extension into Google Chrome as an Unpacked Extension:
+      
+      - Navigate to (1a) chrome://extensions
+        Or
+        (1b) select Menu > More Tools > Extensions.
+        
+      - Enable the (2) developer mode at top right.  
+   
+      - Click (3) "Load Unpacked Extension".
+      
+      - Navigate to the cloned folder and select Ok.  
+          For additional help, please refer to [Official Guide for Chrome](https://developer.chrome.com/extensions/getstarted#unpacked)  
+
+          ![How to install](https://github.com/Sandesh10/Scoreboard/blob/master/howto.png)
+          
+### Local Development
+ - Follow the installation instructions and install the extension.
+   
+ - Make necessary changes in the code.
+   
+ - Use Reload (Ctrl+R) to reload the extension from chrome://extensions page.
+   
+ - Test the new changes.
+ 
+## Contributing
+
+I :heart: contributions. Feel free to send a PR.
+  1. Create an issue if there is one.
+  2. Fork the repo.
+  3. Create your feature branch (`git checkout -b your-feature`).
+  4. Add and commit your changes (`git commit -m 'message'`).
+  5. Push the branch (`git push origin your-feature`).
+  6. Create a new Pull Request.   
+   
+### Feedback
+
+  [Create an issue](https://github.com/Sandesh10/Scoreboard/issues) for all discussions, bugs, feature requests and pull requests.
+  
+### License
+  This project is licensed under the GNU License - see the [LICENSE](https://github.com/Sandesh10/Scoreboard/blob/master/LICENSE) file for details.
--- script.js
+++ script.js
@@ -1,105 +1,137 @@
-// list of countries with their abbrevation  
+// list of countries with their abbrevation used to display the country flag when match has not started 
 var country_keys = '{"Argentina":"ar","France":"fr","Uruguay":"uy","Portugal":"pt",'+
                     '"Spain":"es","Russia":"ru","Croatia":"hr","Denmark":"dk",'+
                     '"Brazil":"br","Mexico":"mx","Belgium":"be","Japan":"jp",'+
                     '"Switzerland":"ch","Sweden":"se","Colombia":"co","England":"gb"}';
+
 
 var getJSON = function(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', url, true);
     xhr.responseType = 'json';
     xhr.onload = function() {
-      var status = xhr.status;
-      if (status === 200) {
-        callback(null, xhr.response);
-      } else {
-        callback(status, xhr.response);
-      }
-    };
+        var status = xhr.status;
+        if (status === 200) {
+            callback(null, xhr.response);
+        } else {
+            callback(status, xhr.response);
+        }
+      };
     xhr.send();
 };
 
+
+
 getJSON('https://worldcup.sfg.io/matches/today',
 function(err, data) {
-  if (err !== null) {
-    alert('Something went wrong: ' + err);
-  } else {
-    if(!data.length){
-      var text ='<div style="padding:5px;font-size:18px;text-align:center;">No Matches Today</div>'
-      document.getElementById("match").innerHTML=text;
-    }else{
-    for(i = 0; i<data.length; i++){
-    var play_time = data[i].time;
-    var game_start= false;
-    var text ='<div class="row">';
-  text +='<div class="column" style="float: left; display: inline;">';
-  if(play_time!=null){
-    game_start = true;
-    if(!play_time.includes("full-time")){
-      text +='<div><button style="vertical-align:middle" type="radio" class="flash-button"/></div>';
-    }
+    if (err !== null) {
+        alert('Something went wrong: ' + err);
+    } else {
+        
+        if(!data.length){
+            var text ='<div class="no_match_today">No Matches Today</div>'
+            document.getElementById("match").innerHTML=text;
+        
+        }else{
+            
+            for(i = 0; i<data.length; i++){
+                var play_time = data[i].time;
+                var game_start= false;
+                var text ='<div class="row">';
+                
+                text +='<div class="column">';
+                
+                if(play_time!=null){
+                    game_start = true;
+                    if(!play_time.includes("full-time")){
+                        text +='<div><button style="vertical-align:middle" type="radio" class="flash-button"/></div>';
+                    }else{
+                        text +='<div>&nbsp;&nbsp;&nbsp;</div>';
+                    }
+                }
 
-  }
-  text +='<div class="match_status" style= "margin-left:4%; background-color:#081f2c;height:1.9em;min-width: 2em;text-align: center;float: left;color: #fff;position: absolute;top: .5em;">';
+                text +='<div class="match_status" style= "">';
     
-    if(game_start == true){   
-      if(play_time == 'full-time'){play_time= "FT";}
-      text +='<span style="padding: 0 5px">'+play_time+'</span>';  
-    }else{
-      ptime = data[i].datetime.split("T")[1].split("Z")[0].slice(0,5);
-      start_time = convert_time(ptime)
-      text +='<span style="padding: 0 5px">'+start_time+'</span>';
-    }
-  var country_code = JSON.parse(country_keys)  
-  text +='</div>';
-  text +='</div>';
-  text +='<div class="column" style="position:relative; margin-left:10%;float:left;width:80%;color:black">';
-  text +='<div class="team_home" style="float:left;width: 48%">';
-    text +='<div style="width:100%; float:right;text-align:right"><span class="team" id="home_team" \
-    style="width:70%;text-align: right;font-size: 1em;">'+ data[i].home_team_country +'</span>';
-    // text +='<br><span>'+data[i].home_team_events[0].player+'</span>'
-    if(!game_start){
-      var team1 = data[i].home_team_country;
-      sub = country_code[team1];
-      text +='<span class="flag-icon flag-icon-'+sub+'" style="float:right;margin-left:0.6em; font-size:1.5em"></span>';
-    }else{  
-    text +='<div style="float:right;margin-left:1em;"><span class="goals" style="font-size: 1em; background-color: #FFF; border: none;height: 1.8em;min-width: 1.8em; color: black;  text-align: center; text-decoration: none;display: inline-block;margin-top:-1%">'+data[i].home_team.goals+'</span></div>';
-    }
-    text +='</div></div><span style="margin-left:2%;width:1%">-</span>';
-    text +='<div class="team_away" style="float:right; width: 45%">';
-    text +='<div style="width:100%; float:right; text-align:left">';
-      if(!game_start){
-      var team2 = data[i].away_team_country;
-      sub = country_code[team2]; 
-      text +='<span class="flag-icon flag-icon-'+sub+'" style="float:left;margin-right:0.6em;font-size:1.5em"></span>';
-    }else{  
-    text +='<div style="float:left;margin-right:1em;"><span class="goals" style="font-size: 1em; background-color: #FFF; border: none;height: 1.8em;width: 1.8em; color: black;  text-align: center; text-decoration: none;display: inline-block;margin-top:-1%">'+
-    data[i].away_team.goals+'</span></div>';
-    } 
-    text +='<span class="team" id="away_team" style="width:70%;text-align: left;font-size: 1em;">'+ data[i].away_team_country +'</span>';
-    text +='</div>';
-    text +='</div>';  
-  text +='</div></div>';
-  text +='<hr style="color:#fff;padding:0; margin:0">'
-  var data_id="match"
-  if(i>0 & i<data.length){
-    data_id+=i;
-  } 
-  document.getElementById(data_id).innerHTML=text;
+                if(game_start == true){   
+                    if(play_time == 'full-time'){
+                      play_time= "FT";
+                    }
+                    text +='<span style="padding: 0 5px">'+play_time+'</span>';  
+                }else{
+                    ptime = data[i].datetime.split("T")[1].split("Z")[0].slice(0,5);
+                    start_time = convert_time(ptime)
+                    text +='<span style="padding: 0 5px">'+start_time+'</span>';
+                }
+                
+                var country_code = JSON.parse(country_keys)  
+                
+                text +='</div>';
+                text +='</div>';
+                text +='<div class="score_column">';
+                text +='<div class="team_home">';
+                text +='<div class="team"><span id="home_team">'+ data[i].home_team_country +'</span>';
+                // text +='<br><span>'+data[i].home_team_events[0].player+'</span>'
+                
+                if(!game_start){
+                    var team1 = data[i].home_team_country;
+                    sub = country_code[team1];
+                    text +='<span class="flag-icon flag-icon-'+sub+'" style="float:right;margin-left:0.6em; font-size:1.5em"></span>';
+                }else{  
+                    text +='<div style="float:right;margin-left:1em;"><span class="goals" style="font-size: 1em; background-color: #FFF; \
+                            border: none;height: 1.8em;min-width: 1.8em; color: black;  text-align: center; text-decoration: none; \
+                            display: inline-block; margin-top:-1%">'+data[i].home_team.goals+'</span></div>';
+                     
+                     // if (data[i].home_team_events.length>0){
+                     //      for(j=0;j<data[i].home_team_events.length;j++){
+                     //         if(data[i].home_team_events[j].type_of_event =="goal"){
+                     //         // document.getElementsByClassName("row").setAttribute("style","height = 4em;");  
+                     //         text +='<br><span style="font-size:0.6em;">'+ data[i].home_team_events[j].player +'</span>';
+                     //    }}}
+                }
+                
+                text +='</div></div><span style="margin-left:2%;width:1%">-</span>';
+                text +='<div class="team_away" style="float:right; width: 45%">';
+                text +='<div style="width:100%; float:right; text-align:left">';
+                
+                if(!game_start){
+                    var team2 = data[i].away_team_country;
+                    sub = country_code[team2]; 
+                    text +='<span class="flag-icon flag-icon-'+sub+'" style="float:left;margin-right:0.6em;font-size:1.5em"></span>';
+                }else{  
+                    text +='<div style="float:left;margin-right:1em;"><span class="goals" style="font-size: 1em; background-color: #FFF; \
+                            border: none;height: 1.8em;width: 1.8em; color: black;  text-align: center; text-decoration: none; \
+                            display: inline-block; margin-top:-1%">'+data[i].away_team.goals+'</span></div>';
+                } 
+                
+                text +='<span id="away_team">'+ data[i].away_team_country +'</span>';
+                text +='</div>';
+                text +='</div>';  
+                text +='</div></div>';
+                text +='<hr style="color:#fff;padding:0; margin:0">'
+                
+                var data_id="match"
+                
+                if(i>0 & i<data.length){
+                    data_id+=i;
+                } 
+                
+                document.getElementById(data_id).innerHTML=text;
+            }
+        } //inner else ends here 
+    } //outer else ends here
+}); //end of function
 
-  }
-  }}
-});
+
 
 function convert_time(time){
-  hr = time.split(":")[0]
-  mm = time.split(":")[1]
-  num_hr = parseInt(hr)+5
-  num_mm = parseInt(mm)+45
-  if(num_mm>=60){
-    num_hr +=1
-    num_mm = num_mm%60
-  }
-  val = num_hr+":"+num_mm
-  return val
+    hr = time.split(":")[0]
+    mm = time.split(":")[1]
+    num_hr = parseInt(hr)+5
+    num_mm = parseInt(mm)+45
+    if(num_mm>=60){
+        num_hr +=1
+        num_mm = num_mm%60
+    }
+    val = num_hr+":"+num_mm
+    return val
 }
