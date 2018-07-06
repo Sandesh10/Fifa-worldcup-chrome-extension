bdede592ff775b9a47fb41f9655c5b327f8aaed0
Sandesh Timilsina <timilsina.sandesh@gmail.com>
2018-07-06 11:25:36 +0545

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,7 @@ var getJSON = function(url, callback) {
 
 
 
-getJSON('https://worldcup.sfg.io/matches/tomorrow',
+getJSON('https://worldcup.sfg.io/matches/today',
 function(err, data) {
     if (err !== null) {
         alert('Something went wrong: ' + err);
